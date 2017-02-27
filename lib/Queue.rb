60012ca484bf50ae2086791d519cbf0163434e6b
ashtn <ahstonehorton@gmail.com>
2017-02-27 00:20:41 -0800

    All tests passed for both files

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,39 @@
 class Queue
+attr_accessor :store
+
   def initialize
+    @store = Array.new
   end
 
   def enqueue(element)
+    @store.unshift << element
   end
 
   def dequeue
+    @store.shift
   end
 
   def front
+    return @store.first
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    if @store.length < 1
+      return true
+    else
+      return false
+    end
   end
 
   def to_s
+    return @store.to_s
   end
 end
+
+# q = Queue.new
+# puts q.size
+# puts q.empty?
--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,27 @@ class Stack
   end
 
   def push(element)
+    @store.push(element)
   end
 
   def pop
+    # last_element = @store.last
+    # @store.delete(@store.last)
+    return @store.pop
   end
 
   def top
+    @store.last
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+  #return @store.length == 0
+  return size == 0
+  #return @store.empty?
   end
 
   def to_s
